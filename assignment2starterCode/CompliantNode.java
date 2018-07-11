cbb87bbe5176518e691677520f2751cc68bcb74e
frozenL <frozenL@users.noreply.github.com>
2018-07-11 16:35:53 +0100

    scored 81/100

--- assignment2starterCode/CompliantNode.java
+++ assignment2starterCode/CompliantNode.java
@@ -1,26 +1,40 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private double p_graph;
+    private double p_malicious;
+    private double p_txDistribution;
+    private int numRounds;
+    private HashSet<Transaction> transactions;
+    private boolean[] followees;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees.clone();
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.transactions = new HashSet<Transaction>(pendingTransactions);
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        return this.transactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        for(Candidate can: candidates) {
+            transactions.add(can.tx);
+        }
     }
 }
